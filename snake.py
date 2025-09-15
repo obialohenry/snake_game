f99163cdbad9b2bb4ff2d6c6653a70fa9caddc9b
sisu <obilaorchisomebi123@gmail.com>
2025-09-15 13:03:35 +0100

    Add comments to snake class methods

--- snake.py
+++ snake.py
@@ -14,6 +14,11 @@ class Snake:
         self.head = self.snake_body_segments[0]
 
     def create_snake(self):
+        """Create the initial snake body.
+
+        The snake starts with three white square turtle segments,
+        each spaced -20 units apart along the x-axis.
+        """
         for body_segment_index in range(3):
             new_body_segment = Turtle(shape='square')
             new_body_segment.color('white')
@@ -23,6 +28,11 @@ class Snake:
             self.snake_body_segments.append(new_body_segment)
 
     def move(self):
+        """Move the snake forward by one step.
+
+        Each segment takes the position of the segment ahead of it,
+        and finally the head moves forward by 20 units.
+        """
         for seg_num in range(len(self.snake_body_segments) - 1, 0, -1):
             new_x = self.snake_body_segments[seg_num - 1].xcor()
             new_y = self.snake_body_segments[seg_num - 1].ycor()
@@ -30,18 +40,22 @@ class Snake:
         self.head.forward(MOVE_DISTANCE)
 
     def up(self):
+        """Turn the snake upward, unless it is currently facing down. """
         if self.head.heading() != DOWN:
             self.head.setheading(UP)
 
     def right(self):
+        """Turn the snake right, unless it is currently facing left. """
         if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)
 
     def left(self):
+        """Turn the snake left, unless it is currently facing right. """
         if self.head.heading() != RIGHT:
             self.head.setheading(LEFT)
 
     def down(self):
+        """Turn the snake downward, unless it is currently facing up. """
         if self.head.heading() != UP:
             self.head.setheading(DOWN)
 
